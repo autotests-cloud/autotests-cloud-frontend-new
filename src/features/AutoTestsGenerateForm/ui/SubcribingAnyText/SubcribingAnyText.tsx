449c0243474dfa6543115163772fcf6c13538284
Nikita <nikitaelin455@gmail.com>
2022-07-20 12:46:25 +0300

    Styling Subscribe Components

--- src/features/AutoTestsGenerateForm/ui/SubcribingAnyText/SubcribingAnyText.tsx
+++ src/features/AutoTestsGenerateForm/ui/SubcribingAnyText/SubcribingAnyText.tsx
@@ -1,15 +1,10 @@
 import { IFormSubscribing } from '../Subscribing.types'
 import styles from './SubcribingAnyText.module.scss'
 import { Typography } from 'antd'
-
 const { Text } = Typography
 
 const SubcribingAnyText: React.FC<IFormSubscribing> = content => {
-	return (
-		<div className={styles.box}>
-			<Text className={styles.text}>{content.content}</Text>
-		</div>
-	)
+	return <Text className={styles.text}>{content.content}</Text>
 }
 
 export default SubcribingAnyText
--- src/features/AutoTestsGenerateForm/ui/SubcribingTelegram/Telegram.tsx
+++ src/features/AutoTestsGenerateForm/ui/SubcribingTelegram/Telegram.tsx
@@ -1,12 +1,13 @@
 import { IFormSubscribing } from '../Subscribing.types'
 import styles from './Telegram.module.scss'
 import { Typography } from 'antd'
+import FadeIn from 'react-fade-in'
 
 const { Text } = Typography
 
 export const Telegram: React.FC<IFormSubscribing> = content => {
 	return (
-		<>
+		<FadeIn>
 			<div className={styles.box}>
 				<Text className={styles.title}>Telegram Notification</Text>
 			</div>
@@ -14,6 +15,6 @@ export const Telegram: React.FC<IFormSubscribing> = content => {
 				className={styles.iframe}
 				src={`https://t.me/${content.content}?embed=1&discussion=1&comments_limit=5&dark=1`}
 			></iframe>
-		</>
+		</FadeIn>
 	)
 }
